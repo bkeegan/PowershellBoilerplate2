472ad1195adf7c8c39b0613b0bc713a7c72dc2ad
bkeegan <brenton.keegan@gmail.com>
2014-10-30 22:44:34 -0400

    Update Get-WindowsFirewall.ps1

--- WindowsOS/Get-WindowsFirewall.ps1
+++ WindowsOS/Get-WindowsFirewall.ps1
@@ -45,76 +45,85 @@ function Get-WindowsFirewall
 		[switch]$public
 		
 	)
-	
-	if(($global -eq $false) -and ($domain -eq $false) -and ($private -eq $false) -and ($public -eq $false))
+	Begin
 	{
-		Throw "You must specify at least one set of values to return. Use -g for global values, -d for domain profile values, -pri for private profile values, -pub for public profile values"
+		if(($global -eq $false) -and ($domain -eq $false) -and ($private -eq $false) -and ($public -eq $false))
+		{
+			Throw "You must specify at least one set of values to return. Use -g for global values, -d for domain profile values, -pri for private profile values, -pub for public profile values"
+		}
 	}
-	
-	Invoke-Command -Computer $computer -ScriptBlock {
-		$firewallDetails = New-Object PSObject 
+	Process
+	{
+		$finalResult = Invoke-Command -Computer $computer -ScriptBlock {
+			$firewallDetails = New-Object PSObject 
 
-		If($args[0].IsPresent -eq $true)
-		{
-			$queryResult = netsh advfirewall show global
-			for($i=0;$i -le $queryResult.GetUpperBound(0);$i++)
+			If($args[0].IsPresent -eq $true)
 			{
-				$matches = $null
-				$regexResult = $queryResult[$i] -match "(.+[ ]{2,})(.*)"
-				if($matches.count -eq 3) 
+				$queryResult = netsh advfirewall show global
+				for($i=0;$i -le $queryResult.GetUpperBound(0);$i++)
 				{
-					Add-Member -InputObject $firewallDetails -Type NoteProperty -Name "Global_$($matches[1])" -value $matches[2]
-				}
-	
-			}
-		}
+					$matches = $null
+					$regexResult = $queryResult[$i] -match "(.+[ ]{2,})(.*)"
 
-		If($args[1].IsPresent -eq $true)
-		{
-			$queryResult = netsh advfirewall show domain
-			for($i=0;$i -le $queryResult.GetUpperBound(0);$i++)
-			{
-				$matches = $null
-				$regexResult = $queryResult[$i] -match "(.+[ ]{2,})(.*)"
-				if($matches.count -eq 3) 
-				{
-					Add-Member -InputObject $firewallDetails -Type NoteProperty -Name "Domain_$($matches[1])" -value $matches[2]
-				}
-	
-			}
-		}
+					if($matches.count -eq 3) 
+					{
+						Add-Member -InputObject $firewallDetails -Type NoteProperty -Name "Global_$($matches[1])" -value $matches[2]
+					}
 		
-		if($args[2].IsPresent -eq $true)
-		{
-			$queryResult = netsh advfirewall show private
-			for($i=0;$i -le $queryResult.GetUpperBound(0);$i++)
-			{
-				$matches = $null
-				$regexResult = $queryResult[$i] -match "(.+[ ]{2,})(.*)"
-				if($matches.count -eq 3) 
-				{
-					Add-Member -InputObject $firewallDetails -Type NoteProperty -Name "Private_$($matches[1])" -value $matches[2]
 				}
-	
 			}
-		}
 
-		if($args[3].IsPresent -eq $true)
-		{
-			$queryResult = netsh advfirewall show public
-			for($i=0;$i -le $queryResult.GetUpperBound(0);$i++)
+			If($args[1].IsPresent -eq $true)
 			{
-				$matches = $null
-				$regexResult = $queryResult[$i] -match "(.+[ ]{2,})(.*)"
-				if($matches.count -eq 3) 
+				$queryResult = netsh advfirewall show domain
+				for($i=0;$i -le $queryResult.GetUpperBound(0);$i++)
 				{
-					Add-Member -InputObject $firewallDetails -Type NoteProperty -Name "Public_$($matches[1])" -value $matches[2]
-				}
-	
-			}
-		}
+					$matches = $null
+					$regexResult = $queryResult[$i] -match "(.+[ ]{2,})(.*)"
+					if($matches.count -eq 3) 
+					{
+						$propertyName = $matches[1] -replace "\s+",""
+						Add-Member -InputObject $firewallDetails -Type NoteProperty -Name "Domain_$propertyName" -value $matches[2]
+					}
 		
-		Return $firewallDetails 
-	} -Args $global,$domain,$private,$public 
-	
+				}
+			}
+			
+			if($args[2].IsPresent -eq $true)
+			{
+				$queryResult = netsh advfirewall show private
+				for($i=0;$i -le $queryResult.GetUpperBound(0);$i++)
+				{
+					$matches = $null
+					$regexResult = $queryResult[$i] -match "(.+[ ]{2,})(.*)"
+					if($matches.count -eq 3) 
+					{
+						Add-Member -InputObject $firewallDetails -Type NoteProperty -Name "Private_$($matches[1])" -value $matches[2]
+					}
+		
+				}
+			}
+
+			if($args[3].IsPresent -eq $true)
+			{
+				$queryResult = netsh advfirewall show public
+				for($i=0;$i -le $queryResult.GetUpperBound(0);$i++)
+				{
+					$matches = $null
+					$regexResult = $queryResult[$i] -match "(.+[ ]{2,})(.*)"
+					if($matches.count -eq 3) 
+					{
+						Add-Member -InputObject $firewallDetails -Type NoteProperty -Name "Public_$($matches[1])" -value $matches[2]
+					}
+		
+				}
+			}
+			
+			Return $firewallDetails 
+		} -Args $global,$domain,$private,$public 
+	}
+	End
+	{
+		Write-Output $finalResult
+	}
 }
