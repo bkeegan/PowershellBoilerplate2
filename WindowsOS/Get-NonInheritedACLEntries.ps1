e3f06656e018830fd95e4de6ee6468bf99519c81
bkeegan <brenton.keegan@gmail.com>
2016-04-06 15:18:33 -0400

    Create Get-NonInheritedACLEntries.ps1

---
+++ WindowsOS/Get-NonInheritedACLEntries.ps1
@@ -0,0 +1,51 @@
+<# 
+.SYNOPSIS 
+	Outputs directories with ACL entries that are not inherited.
+.DESCRIPTION 
+	Iterates recursively through a directory and outputs ACL entries that are not inherited. 
+.NOTES 
+    File Name  : Get-NonInheritedACLEntries.ps1
+    Author     : Brenton keegan - brenton.keegan@gmail.com 
+    Licenced under GPLv3  
+.LINK 
+	https://github.com/bkeegan/PowerShellBoilerplate2
+    License: http://www.gnu.org/copyleft/gpl.html
+.EXAMPLE 
+	Get-NonInheritedACLEntries -f "\\contoso.com\root\share"
+
+#> 
+Function Get-NonInheritedACLEntries
+{
+	[cmdletbinding()]
+	Param
+	(
+		[parameter(Mandatory=$true)]
+		[alias("f")]
+		[string]$folderToCheck
+	)
+
+	$ACLsToCheck = get-childitem $folderToCheck -directory -r -ErrorAction SilentlyContinue | %{get-ACL $_.FullName} -ErrorAction SilentlyContinue | where {$_.Access.IsInherited -eq $false}
+	$ACLResults = @()
+	
+	Foreach($ACL in $ACLsToCheck)
+	{
+
+		$currentPath = $ACL.Path -replace "Microsoft.PowerShell.Core\\FileSystem::",""
+		Foreach ($ACLEntry in $ACL.Access)
+		{
+			If ($ACLEntry.IsInherited -eq $false)
+			{
+				$folderWithACL = New-Object PSObject    
+				$folderWithACL | Add-Member -NotePropertyName "Path" -NotePropertyValue $currentPath
+				$folderWithACL | Add-Member -NotePropertyName "ACL Entry" -NotePropertyValue $ACLEntry.IdentityReference
+				$folderWithACL | Add-Member -NotePropertyName "Rights" -NotePropertyValue $ACLEntry.FileSystemRights
+				$ACLResults += $folderWithACL
+			}
+		}
+	}
+
+	$ACLResults
+}
+
+
+Get-NonInheritedACLEntries -f "\\limcollege.edu\root\Public" | Export-CSV "C:\temp\PubACL.csv" -NoTypeInformation
